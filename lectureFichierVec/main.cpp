355fcf0e30d8831ca06ceaa3bb001dd005992a19
Lac Louis <lac.louis5@gmail.com>
2017-05-09 19:37:49 +0200

    Initial Commit

---
+++ lectureFichierVec/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  lectureFichierVec
+//
+//  Created by Louis Lac on 09/05/2017.
+//  Copyright © 2017 Louis Lac. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
